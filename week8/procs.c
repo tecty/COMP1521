c9316019c6f469a0361b2d2787aef278b1fb314a
Toby Hunag <619058900@qq.com>
2017-10-16 15:48:50 +1100

    finish this weeks' lab

--- week8/procs.c
+++ week8/procs.c
@@ -1,5 +1,5 @@
 // COMP1521 17s2 Lab08 ... processes competing for a resource
- 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -10,15 +10,34 @@
 
 void copyInput(char *);
 
+void nothing(int sig) {
+    /* donothing */
+    if (sig == SIGINT) {
+        /* code */
+        printf("donothing to keyboard interrupt.");
+    }
+}
+
+void ignore(){
+    // set the SIGINT to do nothing
+    printf("Try to ignore some signal\n" );
+    if (signal(SIGINT,nothing)  ) {
+        printf("Coundln't catch keyboard interrupt\n" );
+    }
+}
+
 int main(void)
 {
    struct sigaction act;
    memset (&act, 0, sizeof(act));
 
    if (fork() != 0) {
+       ignore();
       copyInput("Parent");
+      // ignore the keyboard interrupt
    }
    else if (fork() != 0) {
+       ignore();
       copyInput("Child");
    }
    else {
@@ -34,7 +53,8 @@ void copyInput(char *name)
    printf("%s (%d) ready\n", name, mypid);
    while (fgets(line, MAXLINE, stdin) != NULL) {
       printf("%s: %s", name, line);
-      sleep(random()%3);
+    //   sleep(random()%3);
+      sleep(1);
    }
    printf("%s quitting\n", name);
    return;
