c46e791956af4658dec3a675ec8db6ac4f5f41fb
Toby Huang <619058900@qq.com>
2017-10-31 20:11:13 +1100

    setup the environment

---
+++ PPEx1/rmOdd/q1.s
@@ -0,0 +1,39 @@
+# COMP1521 Practice Prac Exam #1
+# int rmOdd(int *src, int n, int*dest)
+
+   .text
+   .globl rmOdd
+
+# params: src=$a0, n=$a1, dest=$a2
+rmOdd:
+# prologue
+   addi $sp, $sp, -4
+   sw   $fp, ($sp)
+   la   $fp, ($sp)
+   addi $sp, $sp, -4
+   sw   $ra, ($sp)
+   addi $sp, $sp, -4
+   sw   $s0, ($sp)
+   addi $sp, $sp, -4
+   sw   $s1, ($sp)
+   # if you need to save more $s? registers
+   # add the code to save them here
+
+# function body
+# locals: ...
+
+   # add code for your rmOdd function here
+
+# epilogue
+   # if you saved more than two $s? registers
+   # add the code to restore them here
+   lw   $s1, ($sp)
+   addi $sp, $sp, 4
+   lw   $s0, ($sp)
+   addi $sp, $sp, 4
+   lw   $ra, ($sp)
+   addi $sp, $sp, 4
+   lw   $fp, ($sp)
+   addi $sp, $sp, 4
+   j    $ra
+
