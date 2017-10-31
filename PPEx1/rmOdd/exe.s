7f20406c6c37f3f21752b3342d5d706da2f736ee
Toby Huang <619058900@qq.com>
2017-10-31 20:47:33 +1100

    finish q1

---
+++ PPEx1/rmOdd/exe.s
@@ -0,0 +1,214 @@
+# COMP1521 Practice Prac Exam #1
+# arrays
+
+   .data
+
+a1:
+   .word   1, 1, 1, 1, 1
+a1N:
+   .word   5      # int a1N = 5
+a2:
+   .space  20     # int a2[5]
+a2N:
+   .word   0      # int a2N = 0
+
+   .align  2
+# COMP1521 Practice Prac Exam #1
+# main program + show function
+
+   .data
+m1:
+   .asciiz "a1 = "
+m2:
+   .asciiz "a2 = "
+   .align  2
+
+   .text
+   .globl main
+main:
+   addi $sp, $sp, -4
+   sw   $fp, ($sp)
+   la   $fp, ($sp)
+   addi $sp, $sp, -4
+   sw   $ra, ($sp)
+
+   la   $a0, m1
+   li   $v0, 4
+   syscall           # printf("a1 = ")
+   la   $a0, a1
+   lw   $a1, a1N
+   jal  showArray    # showArray(a1, a1N)
+
+   la   $a0, a1
+   lw   $a1, a1N
+   la   $a2, a2
+   jal  rmOdd        # a2N = rmOdd(a1, a1N, a2)
+   sw   $v0, a2N
+
+
+   la   $a0, m1
+   li   $v0, 4
+   syscall           # printf("a1 = ")
+   la   $a0, a1
+   lw   $a1, a1N
+   jal  showArray    # showArray(a1, a1N)
+
+   la   $a0, m2
+   li   $v0, 4
+   syscall           # printf("a2 = ")
+   la   $a0, a2
+   lw   $a1, a2N
+   jal  showArray    # showArray(a2, a2N)
+
+   lw   $ra, ($sp)
+   addi $sp, $sp, 4
+   lw   $fp, ($sp)
+   addi $sp, $sp, 4
+   j    $ra
+
+# params: a=$a0, n=$a1
+# locals: a=$s0, n=$s1, i=$s2
+showArray:
+   addi $sp, $sp, -4
+   sw   $fp, ($sp)
+   la   $fp, ($sp)
+   addi $sp, $sp, -4
+   sw   $ra, ($sp)
+   addi $sp, $sp, -4
+   sw   $s0, ($sp)
+   addi $sp, $sp, -4
+   sw   $s1, ($sp)
+   addi $sp, $sp, -4
+   sw   $s2, ($sp)
+
+   move $s0, $a0
+   move $s1, $a1
+   li   $s2, 0            # i = 0
+show_for:
+   bge  $s2, $s1, end_show_for
+
+   move $t0, $s2
+   mul  $t0, $t0, 4
+   add  $t0, $t0, $s0
+   lw   $a0, ($t0)
+   li   $v0, 1            # printf("%d",a[i])
+   syscall
+
+   move $t0, $s2
+   addi $t0, $t0, 1
+   bge  $t0, $s1, incr_show_for
+   li   $a0, ','
+   li   $v0, 11           # printf(",")
+   syscall
+
+incr_show_for:
+   addi $s2, $s2, 1       # i++
+   j    show_for
+
+end_show_for:
+   li   $a0, '\n'
+   li   $v0, 11
+   syscall
+
+   lw   $s2, ($sp)
+   addi $sp, $sp, 4
+   lw   $s1, ($sp)
+   addi $sp, $sp, 4
+   lw   $s0, ($sp)
+   addi $sp, $sp, 4
+   lw   $ra, ($sp)
+   addi $sp, $sp, 4
+   lw   $fp, ($sp)
+   addi $sp, $sp, 4
+   j    $ra
+# COMP1521 Practice Prac Exam #1
+# int rmOdd(int *src, int n, int*dest)
+
+   .text
+   .globl rmOdd
+
+# params: src=$a0, n=$a1, dest=$a2
+rmOdd:
+# prologue
+   addi $sp, $sp, -4
+   sw   $fp, ($sp)
+   la   $fp, ($sp)
+   addi $sp, $sp, -4
+   sw   $ra, ($sp)
+   addi $sp, $sp, -4
+   sw   $s0, ($sp)
+   addi $sp, $sp, -4
+   sw   $s1, ($sp)
+   # if you need to save more $s? registers
+   # add the code to save them here
+
+# function body
+# locals: ...
+
+    # add code for your rmOdd function here
+    # set up the i = 0 j = 0
+    move $t0, $0
+    move $t1, $0
+
+    # set up t2 = 2
+    addi $t2, $0, 2 
+    # set up the return value
+    move $t6, $0
+
+forLoop:
+    # set up the for loop
+    bge $t0, $a1, forLoopEnd
+
+    # calculate the location of the array
+    mul $t3, $t0, 4
+    add $t3, $t3, $a0
+
+    # get the number in t3,
+    lw $t3, ($t3)
+
+    # get the mod of t3
+    div $t3, $t2
+    mfhi $t4
+
+    # if it isn't odd, continue
+    bnez $t4, ifEnd
+    
+    # t3 is odd 
+    
+    # calculate the location of the array
+    mul $t4, $t1, 4
+    add $t4, $t4, $a2
+
+    # save the thing in the array
+    sw $t3, ($t4)
+
+    # increament of j
+    addi $t1, $t1, 1
+
+ifEnd:
+
+
+
+    addi $t0, $t0, 1
+    j forLoop
+forLoopEnd:
+
+    # set up the return value
+    move $v0, $t1
+
+
+
+
+# epilogue
+   # if you saved more than two $s? registers
+   # add the code to restore them here
+   lw   $s1, ($sp)
+   addi $sp, $sp, 4
+   lw   $s0, ($sp)
+   addi $sp, $sp, 4
+   lw   $ra, ($sp)
+   addi $sp, $sp, 4
+   lw   $fp, ($sp)
+   addi $sp, $sp, 4
+   j    $ra
+
--- PPEx1/rmOdd/q1.s
+++ PPEx1/rmOdd/q1.s
@@ -22,7 +22,59 @@ rmOdd:
 # function body
 # locals: ...
 
-   # add code for your rmOdd function here
+    # add code for your rmOdd function here
+    # set up the i = 0 j = 0
+    move $t0, $0
+    move $t1, $0
+
+    # set up t2 = 2
+    addi $t2, $0, 2 
+    # set up the return value
+    move $t6, $0
+
+forLoop:
+    # set up the for loop
+    bge $t0, $a1, forLoopEnd
+
+    # calculate the location of the array
+    mul $t3, $t0, 4
+    add $t3, $t3, $a0
+
+    # get the number in t3,
+    lw $t3, ($t3)
+
+    # get the mod of t3
+    div $t3, $t2
+    mfhi $t4
+
+    # if it isn't odd, continue
+    bnez $t4, ifEnd
+    
+    # t3 is odd 
+    
+    # calculate the location of the array
+    mul $t4, $t1, 4
+    add $t4, $t4, $a2
+
+    # save the thing in the array
+    sw $t3, ($t4)
+
+    # increament of j
+    addi $t1, $t1, 1
+
+ifEnd:
+
+
+
+    addi $t0, $t0, 1
+    j forLoop
+forLoopEnd:
+
+    # set up the return value
+    move $v0, $t1
+
+
+
 
 # epilogue
    # if you saved more than two $s? registers
