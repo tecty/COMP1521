31f815e5b68e90f5ac3881ea381f21a357a0cca7
Toby Huang <619058900@qq.com>
2017-11-07 17:21:05 +1100

    finish get student

--- PPEx2/getStudents/q2/Students.c
+++ PPEx2/getStudents/q2/Students.c
@@ -25,7 +25,19 @@ typedef struct _students {
 // build a collection of student records from a file descriptor
 Students getStudents(int in)
 {
-	return NULL;  // TODO ... replace this by your code
+	students_t *ss = malloc(sizeof(students_t));
+    ss->nstu = 0; 
+    ss->recs = malloc(sizeof(sturec_t));
+    int nwords = 0;
+
+    while ((nwords=read(in, &ss->recs[ss->nstu], sizeof(sturec_t))) == sizeof(sturec_t)) {
+        
+        ss->nstu++;
+        ss->recs = realloc(ss->recs,(ss->nstu+1)*sizeof(sturec_t));
+    }
+
+    
+    return ss;  // TODO ... replace this by your code
 }
 
 // show a list of student records pointed to by ss
